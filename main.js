28ea2edcf966d96ab1beec74c9506200a5940ae4
Max Keybr <maximefim@gmail.com>
2020-08-23 02:25:04 +0700

    Homework-3

--- main.js
+++ main.js
@@ -1,45 +1,67 @@
-//1 задача
+const $btn = document.getElementById("btn-kick");
+const $lowBtn = document.getElementById("btn-low-kick");
 
-const firstRow = "мама мыла раму";
-const secondRow = "собака друг человека";
+const character = {
+  name: "Pikahu",
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById("health-character"),
+  elProgressbar: document.getElementById("progressbar-character"),
+};
 
-function getRow(firstRow, secondRow) {
-  let counterFirst = 0;
-  let counterSecond = 0;
-  for (let i = 0; i < firstRow.length; i++) {
-    if (firstRow.charAt(i) == "а") {
-      counterFirst++;
-    }
-  }
-  for (let i = 0; i < secondRow.length; i++) {
-    if (secondRow.charAt(i) == "а") {
-      counterSecond++;
-    }
-  }
-  if (counterFirst > counterSecond) {
-    console.log(firstRow);
-  } else {
-    console.log(secondRow);
-  }
+const enemy = {
+  name: "Charmander",
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById("health-enemy"),
+  elProgressbar: document.getElementById("progressbar-enemy"),
+};
+
+$btn.addEventListener("click", function () {
+  console.log("Kick");
+  changeHP(random(20), character);
+  changeHP(random(20), enemy);
+});
+
+$lowBtn.addEventListener("click", function () {
+  console.log("LowKick");
+  changeHP(random(10), character);
+  changeHP(random(10), enemy);
+});
+
+function init() {
+  console.log("Start Game!");
+  renderHP(character);
+  renderHP(enemy);
 }
-getRow(firstRow, secondRow);
 
-//2 задача // +7 (123) 456-78-90
+function renderHP(person) {
+  renderHPLife(person);
+  renderProgressbarHP(person);
+}
 
-// function formattedPhone(phone) {
-//   let phoneNew = "";
-//   for (let i = 0; i < phone.length; i++) {
-//     phoneNew += phone.charAt(i);
-//     if (i == 1) {
-//       phoneNew += phone.charAt(i) + " (";
-//     } else if (i == 4) {
-//       phoneNew += phone.charAt(i) + ") ";
-//     } else if (i == 7) {
-//       phoneNew += phone.charAt(i) + "-";
-//     } else if ((i = 9)) {
-//       phoneNew += phone.charAt(i) + "-";
-//     }
-//   }
-//   return phoneNew;
-// }
-// console.log(formattedPhone("+71234567890"));
+function renderHPLife(person) {
+  person.elHP.innerText = person.damageHP + " / " + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+  person.elProgressbar.style.width = person.damageHP + "%";
+}
+
+function changeHP(count, person) {
+  if (person.damageHP < count) {
+    person.damageHP = 0;
+    alert("Stupid " + person.name + " game over");
+    $btn.disabled = true;
+  } else {
+    person.damageHP -= count;
+  }
+
+  renderHP(person);
+}
+
+function random(num) {
+  return Math.ceil(Math.random() * num);
+}
+
+init();
