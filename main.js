d40a0a02b798316e10311dae9aa209fca135e67d
Efimenko Maksim <mefimenko@smb.net>
2020-09-04 01:20:32 +0700

    inception

--- main.js
+++ main.js
@@ -1,38 +1,34 @@
 import Pokemon from "./pokemon.js";
 import { random, countClickFirst, generateLog } from "./utils.js";
+import { pokemons } from "./pokemons.js";
+
+const pikachu = pokemons.find((item) => item.name === "Pikachu");
 
 const player1 = new Pokemon({
-  name: "Pikachu",
-  damageHP: 400,
-  defaultHP: 400,
-  selectors: "character",
+  ...pikachu,
+  selectors: "player1",
 });
 
 const player2 = new Pokemon({
   name: "Charmander",
   damageHP: 300,
   defaultHP: 300,
-  selectors: "enemy",
+  selectors: "player2",
 });
 
-function $getElById(id) {
-  return document.getElementById(id);
-}
+const $control = document.querySelector(".control");
 
-const $btn = $getElById("btn-kick");
-const $lowBtn = $getElById("btn-low-kick");
-
-const firstKick = countClickFirst(5, $btn);
-const secondKick = countClickFirst(7, $lowBtn);
-
-$btn.addEventListener("click", () => {
-  firstKick();
-  damage(20, 10);
-});
-
-$lowBtn.addEventListener("click", function () {
-  secondKick();
-  damage(10);
+player1.attacks.forEach((item) => {
+  console.log(item);
+  const $btn = document.createElement("button");
+  $btn.classList.add("button");
+  $btn.innerText = item.name;
+  const kickBtn = countClickFirst(item.maxCount, $btn);
+  $btn.addEventListener("click", () => {
+    console.log("Click button ", $btn.innerText);
+    kickBtn();
+  });
+  $control.appendChild($btn);
 });
 
 function damage(num) {
--- pokemon.js
+++ pokemon.js
@@ -2,19 +2,26 @@ class Selectors {
   constructor(name) {
     this.elHP = document.getElementById(`health-${name}`);
     this.elProgressbar = document.getElementById(`progressbar-${name}`);
+    this.pokiName = document.getElementById(`name-${name}`);
   }
 }
 
 class Pokemon extends Selectors {
-  constructor({ name, damageHP, defaultHP, selectors }) {
+  constructor({ name, damageHP, defaultHP, selectors, attacks = [] }) {
     super(selectors);
 
     this.name = name;
     this.damageHP = damageHP;
     this.defaultHP = defaultHP;
+    this.attacks = attacks;
 
     this.renderHP();
+    this.changeName();
   }
+
+  changeName = () => {
+    this.pokiName.innerText = this.name;
+  };
 
   changeHP = (count, cb) => {
     this.damageHP -= count;
